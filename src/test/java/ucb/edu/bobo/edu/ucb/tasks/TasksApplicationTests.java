5adee87b77bcf5701ffb8395299aab246d546763
Laaauuura <104600276+Laaauuura@users.noreply.github.com>
2023-09-26 23:47:06 -0400

    spring y dependencias

---
+++ src/test/java/ucb/edu/bobo/edu/ucb/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.edu.bobo.edu.ucb.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
