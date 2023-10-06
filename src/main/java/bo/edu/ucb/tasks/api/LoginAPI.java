88569d9ddb732cde3a5b2d8af2d94fdf1cd95ee0
Laaauuura <104600276+Laaauuura@users.noreply.github.com>
2023-10-06 13:20:30 -0400

    views y login funcional

--- src/main/java/bo/edu/ucb/tasks/api/LoginAPI.java
+++ src/main/java/bo/edu/ucb/tasks/api/LoginAPI.java
@@ -2,6 +2,7 @@ package bo.edu.ucb.tasks.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import bo.edu.ucb.tasks.dto.ResponseDto;
 import bo.edu.ucb.tasks.entity.Usuario;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginAPI {
 
     private static final Logger LOG = LoggerFactory.getLogger(LoginAPI.class);
